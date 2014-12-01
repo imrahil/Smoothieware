1232906dedb8e75c2dd764d65c31fcf2d57adfed
Jarek Szczepanski <imrahil@imrahil.com>
2014-12-01 22:15:46 +0100

    Fixed actuator_to_cartesian equations. Cleaned a little bit.

--- src/modules/robot/arm_solutions/SimpsonSolution.cpp
+++ src/modules/robot/arm_solutions/SimpsonSolution.cpp
@@ -9,9 +9,7 @@
 #include "libs/Config.h"
 #include "Vector3.h"
 
-#define simpson_effector_height_checksum        CHECKSUM("simpson_effector_height")
-#define simpson_arm_length_checksum             CHECKSUM("simpson_arm_length")
-#define simpson_printable_radius_checksum       CHECKSUM("simpson_printable_radius")
+#define simpson_size_checksum                   CHECKSUM("simpson_size")
 
 #define max_arm_length_x_checksum               CHECKSUM("max_arm_length_x")
 #define max_arm_length_y_checksum               CHECKSUM("max_arm_length_y")
@@ -26,12 +24,8 @@
 
 SimpsonSolution::SimpsonSolution(Config* config)
 {
-    // Approximate Z height from shoulder swivel to wrist hinge (with hot end on bed)
-    simpson_effector_height     = config->value(simpson_effector_height_checksum)->by_default(70.0f)->as_number();
-    // Approximate distance between the centers of the hinge bolts when the end stop is triggered
-    simpson_arm_length          = config->value(simpson_arm_length_checksum)->by_default(300.0f)->as_number();
-    // Actually this is distance from the arm swivel point to bed center. It corresponds to 250mm between any pair of swivel points.
-    simpson_printable_radius    = config->value(simpson_printable_radius_checksum)->by_default(144.37f)->as_number();
+    // Distance between shoulder pivot bolts (i.e. the vertical bolts through the bed)
+    simpson_size    = config->value(simpson_size_checksum)->by_default(250.0f)->as_number();
 
     // For GUS, these MUST be updated with calibrated values
     max_arm_length_x    = config->value(max_arm_length_x_checksum)->by_default(295.0f)->as_number();
@@ -48,14 +42,16 @@ void SimpsonSolution::init() {
     float SIN_60   = 0.8660254037844386F;
     float COS_60   = 0.5F;
 
-    SIMPSON_ARM1_X = -SIN_60 * simpson_printable_radius; // front left arm
-    SIMPSON_ARM1_Y = -COS_60 * simpson_printable_radius;
+    SIMPSON_PRINTABLE_RADIUS = (simpson_size / 2.0) / SIN_60;
+    
+    SIMPSON_ARM1_X = -SIN_60 * SIMPSON_PRINTABLE_RADIUS; // front left arm
+    SIMPSON_ARM1_Y = -COS_60 * SIMPSON_PRINTABLE_RADIUS;
 
-    SIMPSON_ARM2_X =  SIN_60 * simpson_printable_radius; // front right arm
-    SIMPSON_ARM2_Y = -COS_60 * simpson_printable_radius;
+    SIMPSON_ARM2_X =  SIN_60 * SIMPSON_PRINTABLE_RADIUS; // front right arm
+    SIMPSON_ARM2_Y = -COS_60 * SIMPSON_PRINTABLE_RADIUS;
 
     SIMPSON_ARM3_X = 0.0F; // back middle arm
-    SIMPSON_ARM3_Y = simpson_printable_radius;
+    SIMPSON_ARM3_Y = SIMPSON_PRINTABLE_RADIUS;
 }
 
 void SimpsonSolution::cartesian_to_actuator( float cartesian_mm[], float actuator_mm[] )
@@ -77,38 +73,18 @@ void SimpsonSolution::actuator_to_cartesian( float actuator_mm[], float cartesia
     float i = SIMPSON_ARM2_X;
     float j = -SIMPSON_ARM2_Y * 3.0;
     
-    float x = (SQ(actuator_mm[0]) - SQ(actuator_mm[1]) + SQ(d)) / (2.0 * d);
-    float y = (SQ(actuator_mm[0]) - SQ(actuator_mm[2]) - SQ(x) + SQ(x-i) + SQ(j)) / (2.0 * j);
-    float z = sqrtf(SQ(actuator_mm[0]) - SQ(x) - SQ(y));
+    float x = (SQ(max_arm_length_x - actuator_mm[0]) - SQ(max_arm_length_y - actuator_mm[1]) + SQ(d)) / (2.0 * d);
+    float y = (SQ(max_arm_length_x - actuator_mm[0]) - SQ(max_arm_length_z - actuator_mm[2]) - SQ(x) + SQ(x-i) + SQ(j)) / (2.0 * j);
+    float zsq = SQ(max_arm_length_x - actuator_mm[0]) - SQ(x) - SQ(y);
+    float z;
+    
+    if(zsq < 0.0){
+        z = 0.0;
+    }else{
+        z = sqrtf(SQ(max_arm_length_x - actuator_mm[0]) - SQ(x) - SQ(y));
+    }
     
     cartesian_mm[0] = ROUND(x, 4);
     cartesian_mm[1] = ROUND(y, 4);
     cartesian_mm[2] = ROUND(z, 4);
 }
-
-bool SimpsonSolution::set_optional(const arm_options_t& options) {
-
-    arm_options_t::const_iterator i;
-
-    i= options.find('H');
-    if(i != options.end()) {
-        simpson_effector_height= i->second;
-    }
-    i= options.find('L');
-    if(i != options.end()) {
-        simpson_arm_length= i->second;
-    }
-    i= options.find('R');
-    if(i != options.end()) {
-        simpson_printable_radius= i->second;
-    }
-    init();
-    return true;
-}
-
-bool SimpsonSolution::get_optional(arm_options_t& options) {
-    options['H']= this->simpson_effector_height;
-    options['L']= this->simpson_arm_length;
-    options['R']= this->simpson_printable_radius;
-    return true;
-};
--- src/modules/robot/arm_solutions/SimpsonSolution.h
+++ src/modules/robot/arm_solutions/SimpsonSolution.h
@@ -11,15 +11,10 @@ class SimpsonSolution : public BaseSolution {
         void cartesian_to_actuator( float[], float[] );
         void actuator_to_cartesian( float[], float[] );
 
-        bool set_optional(const arm_options_t& options);
-        bool get_optional(arm_options_t& options);
-
     private:
         void init();
 
-        float simpson_effector_height;
-        float simpson_arm_length;
-        float simpson_printable_radius;
+        float simpson_size;
         
         float max_arm_length_x;
         float max_arm_length_y;
@@ -27,6 +22,8 @@ class SimpsonSolution : public BaseSolution {
         float shoulder_height_x;
         float shoulder_height_y;
         float shoulder_height_z;
+
+        float SIMPSON_PRINTABLE_RADIUS;
 
         float SIMPSON_ARM1_X;
         float SIMPSON_ARM1_Y;
